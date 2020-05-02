b2e58a540c636d50d1b4464cf40951963533287d
stldo <git@stldo.dev>
2020-05-02 15:52:05 -0300

    Added external file support

--- packages/gatsby-yaml-full-markdown/index.js
+++ packages/gatsby-yaml-full-markdown/index.js
@@ -1,11 +1,26 @@
+const fs = require('fs')
+const path = require('path')
 const remark = require('remark')
 const remarkHtml = require('remark-html')
+const { promisify } = require('util')
 
-module.exports = () => ({
+const readFile = promisify(fs.readFile)
+
+const NEWLINE_REGEXP = /\r|\n/
+
+module.exports = ({ node }) => ({
   tag: '!markdown',
   options: {
     kind: 'scalar',
     construct: async data => {
+      if (!NEWLINE_REGEXP.test(data)) {
+        const filePath = path.resolve(node.dir, data)
+        data = await readFile(filePath, 'utf8').catch(error => {
+          if (error.code === 'ENOENT') return data
+          throw error
+        })
+      }
+
       return (await remark().use(remarkHtml).process(data)).contents
     },
   },
