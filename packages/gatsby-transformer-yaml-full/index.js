ca333a527a8b2bfb47ea0bd5e54abc917d5f9d09
stldo <st.shared@icloud.com>
2021-01-10 04:32:36 +0000

    Update: JS-YAML 4

--- packages/gatsby-transformer-yaml-full/index.js
+++ packages/gatsby-transformer-yaml-full/index.js
@@ -1,13 +1,13 @@
-const jsYaml = require(`js-yaml`)
+const jsYaml = require('js-yaml')
 
 const MULTI_DOCUMENT_YAML = /^-{3}[ \t]*?($|[#!]|[|>][ \t]*?$)/m
 
 let schema
 
 exports.configureSchema = types => {
-  schema = !types.length
-    ? jsYaml.DEFAULT_FULL_SCHEMA
-    : jsYaml.Schema.create(jsYaml.DEFAULT_FULL_SCHEMA, types)
+  schema = types.length
+    ? jsYaml.DEFAULT_SCHEMA.extend(types)
+    : jsYaml.DEFAULT_SCHEMA
 }
 
 exports.parse = content => (
