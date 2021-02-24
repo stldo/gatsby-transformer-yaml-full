f15a7d587b532ca32a9e5a0289db2e50427c2086
stldo <git@stldo.dev>
2021-02-24 01:43:09 -0300

    Add: load file support

--- packages/gatsby-yaml-full-markdown/index.js
+++ packages/gatsby-yaml-full-markdown/index.js
@@ -1,3 +1,8 @@
+const { readFile } = require('fs/promises')
+const path = require('path')
+
+const NEWLINE_REGEXP = /\n|\r/
+
 module.exports = (
   { actions: { createNode }, createContentDigest, createNodeId, node }
 ) => {
@@ -6,6 +11,16 @@ module.exports = (
     options: {
       kind: 'scalar',
       construct: async content => {
+        if (!NEWLINE_REGEXP.test(content)) { // If the content is single line
+          await readFile(path.resolve(node.dir, content), 'utf8').then(data => {
+            content = data
+          }).catch(error => {
+            if (error.code !== 'ENOENT' && error.code !== 'ENAMETOOLONG') {
+              throw error
+            }
+          })
+        }
+
         const contentDigest = createContentDigest(content)
         const id = createNodeId(`${node.id}.${contentDigest} >>> YAML/MARKDOWN`)
 
@@ -22,6 +37,7 @@ module.exports = (
         }
 
         await createNode(child)
+
         return child
       }
     }
