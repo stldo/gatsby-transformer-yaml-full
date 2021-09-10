b0f32161996496ea06fe3241e35c411e8e54e4c1
stldo <git@stldo.dev>
2021-09-09 23:44:04 -0300

    fix: touch nodes to avoid them being garbage collected

--- packages/gatsby-yaml-full-markdown/gatsby-node.js
+++ packages/gatsby-yaml-full-markdown/gatsby-node.js
@@ -1,28 +1,31 @@
 const { isPlainObject } = require('is-plain-object')
 
-const GATSBY_RESERVED_FIELDS = ['children', 'id', 'internal', 'parent']
+const GATSBY_FIELDS = ['children', 'id', 'internal', 'parent']
 
-function linkMarkdownNodes (node, isNodeRoot = true) {
+function linkMarkdownNodes (node) {
   for (const key in node) {
-    if (
-      (isNodeRoot && GATSBY_RESERVED_FIELDS.includes(key)) ||
-      !isPlainObject(node[key])
-    ) {
+    if (GATSBY_FIELDS.includes(key) || !isPlainObject(node[key])) {
+      continue
+    } else if (node[key].internal?.type !== 'YamlMarkdown') {
+      linkMarkdownNodes(node[key])
       continue
     }
 
-    if ((node[key].internal || {}).type !== 'YamlMarkdown') {
-      linkMarkdownNodes(node[key], false)
-    } else {
-      node[`${key}___NODE`] = node[key].id
-      delete node[key]
-    }
+    node[`${key}___NODE`] = node[key].id
+    delete node[key]
   }
 }
 
 exports.onCreateNode = ({ node }) => {
   const { owner, type } = node.internal
+
   if (owner === 'gatsby-transformer-yaml-full' && type !== 'YamlMarkdown') {
     linkMarkdownNodes(node)
+  }
+}
+
+exports.onPreBootstrap = ({ actions: { touchNode }, getNodesByType }) => {
+  for (const node of getNodesByType('YamlMarkdown')) {
+    touchNode(node)
   }
 }
