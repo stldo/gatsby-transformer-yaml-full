be11cc4ad653aa2f49237a6a8321ed8a9a0cce28
stldo <st.shared@icloud.com>
2021-09-16 15:28:31 -0300

    feat: use js-yaml to parse imported node content directly

--- packages/gatsby-yaml-full-import/index.js
+++ packages/gatsby-yaml-full-import/index.js
@@ -1,58 +1,73 @@
 const path = require('path')
+const jsYaml = require('js-yaml')
 
 const watcher = require('./lib/watcher')
 
 const MULTI_DOCUMENT_YAML = /^-{3}[ \t]*?($|[#!]|[|>][ \t]*?$)/m
+const YAML_TAGS = /(?:^[ \t]*|:[ \t]+)(![^ \t]+)/gm
 
 module.exports = helpers => ({
   tag: '!import',
   options: {
     kind: 'scalar',
     construct: async data => {
-      const { getNode, getNodes, loadNodeContent, node, reporter } = helpers
-      const [importPath, importField] = data.split('!')
-      const importAbsolutePath = path.resolve(node.dir, importPath)
-      const [importNode] = getNodes().filter(({ absolutePath }) => (
-        absolutePath === importAbsolutePath
+      const { getNodes, loadNodeContent, node, reporter } = helpers
+      const [importRelativePath, importField] = data.split('!')
+      const importPath = path.resolve(node.dir, importRelativePath)
+      const [fileNode] = getNodes().filter(({ absolutePath }) => (
+        absolutePath === importPath
       ))
 
-      if (!importNode) {
+      if (!fileNode) {
         reporter.error(
-          `"!import ${importPath}" not found in "${node.relativePath}"`
+          `!import "${importRelativePath}" not found in ` +
+          `"${node.relativePath}"`
         )
+
         return null
       }
 
-      await loadNodeContent(importNode)
-
       if (process.env.NODE_ENV === 'development') {
-        await watcher.add(helpers, importAbsolutePath, node.absolutePath)
+        await watcher.add(helpers, importPath, node.absolutePath)
       }
 
-      const importNodeContent = importNode.internal.content
-      const content = importNodeContent.search(MULTI_DOCUMENT_YAML) !== -1
-        ? importNode.children.map(id => getNode(id))
-        : getNode(importNode.children[0])
+      const yaml = await loadNodeContent(fileNode)
+      const types = []
+
+      let yamltagMatch
+
+      /* Match YAML tags and return their fields as strings */
+      while (yamltagMatch = YAML_TAGS.exec(yaml)) {
+        types.push(new jsYaml.Type(yamltagMatch[1], { kind: 'scalar' }))
+      }
+
+      const jsYamlOptions = types.length
+        ? { schema: jsYaml.DEFAULT_SCHEMA.extend(types) }
+        : {}
+
+      let content = MULTI_DOCUMENT_YAML.test(yaml)
+        ? jsYaml.loadAll(yaml, jsYamlOptions)
+        : jsYaml.load(yaml, jsYamlOptions)
 
       if (!importField) {
         return content
       }
 
-      let errorWasPrinted = false
-
-      return importField.split('.').reduce((accumulator, field) => {
-        if (accumulator && field in accumulator) {
-          return accumulator[field]
-        } else if (errorWasPrinted) {
-          return null
+      for (const field of importField.split('.')) {
+        if (field in content) {
+          content = content[field]
+          continue
         }
 
         reporter.error(
-          `"${importField}" doesn't exist in "!import ${importPath}"`
+          `"${importField}" doesn't exist in ` +
+          `!import "${importRelativePath}"`
         )
 
-        errorWasPrinted = true
-      }, content)
+        return null
+      }
+
+      return content
     }
   }
 })
--- packages/gatsby-yaml-full-import/lib/watcher.js
+++ packages/gatsby-yaml-full-import/lib/watcher.js
@@ -3,12 +3,12 @@ const { utimes } = require('fs/promises')
 
 const CACHE_KEY = 'watchList'
 
-let subPluginCache
+let gatsbyPluginCache
 
 async function watchImportPath (getNodes, importPath) {
   try {
     const watcher = watch(importPath, async () => {
-      const watchList = await subPluginCache.get(CACHE_KEY)
+      const watchList = await gatsbyPluginCache.get(CACHE_KEY)
       const pathList = watchList[importPath]
 
       if (!pathList) {
@@ -22,12 +22,12 @@ async function watchImportPath (getNodes, importPath) {
 
       if (!nodeList.length) {
         watchList[importPath] = null
-        await subPluginCache.set(CACHE_KEY, watchList)
+        await gatsbyPluginCache.set(CACHE_KEY, watchList)
         watcher.close()
         return
       } else if (nodeList.length !== pathList.length) {
         watchList[importPath] = nodeList.map(({ absolutePath }) => absolutePath)
-        await subPluginCache.set(CACHE_KEY, watchList)
+        await gatsbyPluginCache.set(CACHE_KEY, watchList)
       }
 
       const time = new Date()
@@ -43,29 +43,29 @@ async function watchImportPath (getNodes, importPath) {
       throw error
     }
 
-    const watchList = await subPluginCache.get(CACHE_KEY)
+    const watchList = await gatsbyPluginCache.get(CACHE_KEY)
     watchList[importPath] = null
-    await subPluginCache.set(CACHE_KEY, watchList)
+    await gatsbyPluginCache.set(CACHE_KEY, watchList)
   }
 }
 
 exports.add = async ({ getNodes }, importPath, nodePath) => {
-  const watchList = await subPluginCache.get(CACHE_KEY)
+  const watchList = await gatsbyPluginCache.get(CACHE_KEY)
 
   if (!watchList[importPath]) {
     watchList[importPath] = [nodePath]
-    await subPluginCache.set(CACHE_KEY, watchList)
+    await gatsbyPluginCache.set(CACHE_KEY, watchList)
     await watchImportPath(getNodes, importPath)
   } else if (!watchList[importPath].includes(nodePath)) {
     watchList[importPath].push(nodePath)
-    await subPluginCache.set(CACHE_KEY, watchList)
+    await gatsbyPluginCache.set(CACHE_KEY, watchList)
   }
 }
 
 exports.init = async ({ cache, getNodes }) => {
   const watchList = await cache.get(CACHE_KEY)
 
-  subPluginCache = cache
+  gatsbyPluginCache = cache
 
   if (!watchList) {
     await cache.set(CACHE_KEY, {})
