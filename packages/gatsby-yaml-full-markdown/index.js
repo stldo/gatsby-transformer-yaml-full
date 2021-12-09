135903d352e90dce925463ef6be435c9af64c375
stldo <st.shared@icloud.com>
2021-12-09 05:21:14 -0300

    feat: transform content using remark instead of creating a new node

--- packages/gatsby-yaml-full-markdown/index.js
+++ packages/gatsby-yaml-full-markdown/index.js
@@ -1,44 +1,40 @@
 const { readFile } = require('fs/promises')
 const path = require('path')
+const remarkHtml = require('remark-html')
+const remarkParse = require('remark-parse')
+const unified = require('unified')
 
 const NEWLINE_REGEXP = /\n|\r/
 
-module.exports = (
-  { actions: { createNode }, createContentDigest, createNodeId, node }
-) => {
+let remark
+
+module.exports = ({ node }, _, options) => {
   return {
     tag: '!markdown',
     options: {
       kind: 'scalar',
       construct: async content => {
         if (!NEWLINE_REGEXP.test(content)) { // If the content is single line
-          await readFile(path.resolve(node.dir, content), 'utf8').then(data => {
-            content = data
-          }).catch(error => {
-            if (error.code !== 'ENOENT' && error.code !== 'ENAMETOOLONG') {
-              throw error
-            }
-          })
+          await readFile(path.resolve(node.dir, content), 'utf8')
+            .then(data => {
+              content = data
+            }).catch(error => {
+              if (
+                error.code !== 'ENOENT' &&
+                error.code !== 'ENAMETOOLONG'
+              ) {
+                throw error
+              }
+            })
         }
 
-        const contentDigest = createContentDigest(content)
-        const id = createNodeId(`${node.id}.${contentDigest} >>> YAML/MARKDOWN`)
-
-        const child = {
-          id,
-          children: [],
-          parent: null,
-          internal: {
-            content,
-            contentDigest,
-            mediaType: 'text/markdown',
-            type: 'YamlMarkdown'
-          }
+        if (!remark) {
+          remark = unified()
+            .use(remarkParse)
+            .use(remarkHtml, options.remarkHtml)
         }
 
-        await createNode(child)
-
-        return child
+        return String(await remark.process(content))
       }
     }
   }
